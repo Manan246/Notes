16e274addd91b50d2e531a72ef348da140f97f0d
Manan246 <62480713+Manan246@users.noreply.github.com>
2024-02-14 10:03:37 +0530

    Update m.py

--- m.py
+++ m.py
@@ -1,2 +1,3 @@
 det=(matrix[0][0]*((matrix[1][1]*matrix[2][2])-(matrix[2][1]*matrix[1][2])))-(matrix[0][1]*((matrix[1][0]*matrix[2][2])-(matrix[2][0]*matrix[1][2])))+(matrix[0][2]*((matrix[1][0]*matrix[2][1])-(matrix[2][0]*matrix[1][1])))
     return det
+matrix[i][j]=a[np.arange(3)!=i][:,np.arange(3)!=j]
